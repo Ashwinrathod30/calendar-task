b9372c17652ac158a42fe7b7735011fb89ca3ed2
Ashwinrathod30 <ashwinrathod8055@gmail.com>
2025-08-22 15:00:55 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 import Calendar from './components/Calendar';
 import Overview from './pages/Overview';
